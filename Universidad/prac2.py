166310f3fc46fc54b3b68bd147bdbd8e6c5476fe
DHZHZ <Allenzual@gmail.com>
2023-08-29 10:17:39 -0600

    V2

---
+++ Universidad/prac2.py
@@ -0,0 +1,23 @@
+'use stricht'
+
+num = 0
+fact = 1
+i = 1
+respuesta = "SI"
+
+while respuesta.upper() == "SI":
+    while True:
+        try:
+           num = int(input("Escribe un numero: "))
+           break
+        except ValueError:
+            print("Solo datos numericos por favot")
+    i = 1
+    fact = 1
+    while i <= num:
+        fact *= i # fact = fact * i
+        i += 1 #i = i + 1
+    print(f"El factorial de {num} es {fact}")
+    respuesta = input("Deseas repetir el proceso (Si/No): ")
+print("adios")
+    
